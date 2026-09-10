246a0f8d54bc4a78eef082a29a602a395ceacce3
anommanager <anommanager@gmail.com>
2026-09-10 22:53:36 +0500

    refactor: remove magic strings

--- app/src/main/java/org/example/CarDirector.java
+++ app/src/main/java/org/example/CarDirector.java
@@ -1,11 +1,18 @@
 package org.example;
 
 public class CarDirector {
+  private static final String racingCarBrand = "Ferrari";
+  private static final String racingCarModel = "F1";
+  private static final Double racingCarPrice = 2590000.0;
+  private static final String regularCarBrand = "Mercedes";
+  private static final String regularCarModel = "Benz";
+  private static final Double regularCarPrice = 100000.0;
+
   public void makeRacingCar(CarBuilder builder) {
-    builder.setBrand("Ferrari").setModel("X1").setPrice(2590000);
+    builder.setBrand(racingCarBrand).setModel(racingCarModel).setPrice(racingCarPrice);
   }
 
   public void makeRegularCar(CarBuilder builder) {
-    builder.setBrand("Mercedes").setModel("Benz").setPrice(100000);
+    builder.setBrand(regularCarBrand).setModel(regularCarModel).setPrice(regularCarPrice);
   }
 }
